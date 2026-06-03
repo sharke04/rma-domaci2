4906c0ada898511a46ca43583b33cbc3cd6e206a
Sare <nemanjasaric2004a@gmail.com>
2026-06-03 13:02:41 +0200

    sync favourites and watchlist on app start

--- composeApp/src/commonMain/kotlin/rs/edu/raf/rma/showtime/welcome/WelcomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/rs/edu/raf/rma/showtime/welcome/WelcomeViewModel.kt
@@ -9,10 +9,14 @@ import kotlinx.coroutines.launch
 import rs.edu.raf.rma.core.auth.AuthStore
 import rs.edu.raf.rma.core.auth.model.AuthState
 import rs.edu.raf.rma.showtime.domain.ShowtimeRepository
+import rs.edu.raf.rma.showtime.favourites.FavouritesRepository
+import rs.edu.raf.rma.showtime.watchlist.WatchlistRepository
 
 class WelcomeViewModel(
     private val authStore: AuthStore,
     private val showtimeRepository: ShowtimeRepository,
+    private val favouritesRepository: FavouritesRepository,
+    private val watchlistRepository: WatchlistRepository,
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(WelcomeContract.UiState())
@@ -25,11 +29,21 @@ class WelcomeViewModel(
     init {
         observeAuthState()
         refreshMovies()
+        syncCollections()
     }
 
     private fun refreshMovies() {
         viewModelScope.launch {
             showtimeRepository.refreshMovies()
+        }
+    }
+
+    private fun syncCollections() {
+        viewModelScope.launch {
+            favouritesRepository.sync()
+        }
+        viewModelScope.launch {
+            watchlistRepository.sync()
         }
     }
 
